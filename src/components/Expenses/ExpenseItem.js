20d606f90390246bbb4117d8d9b6e8d5906203aa
Yonder Peña <djyonder@gmail.com>
2021-11-01 12:35:23 -0500

    cambio de funciones normales a funciones de flechas

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,7 +4,7 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const  ExpenseItem = (props) => {
 
   return (
     <Card className="expense-item">
