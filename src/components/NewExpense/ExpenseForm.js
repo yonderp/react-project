35cf8ce7244009c71d55d7ddeba8c598ef477280
Yonder Peña <djyonder@gmail.com>
2021-11-09 11:03:03 -0500

    dos maneras de hacer Binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -49,6 +49,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -56,7 +59,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
