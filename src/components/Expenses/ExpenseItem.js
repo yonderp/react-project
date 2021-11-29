fca0a98693d0c70fa89bfbfe6f32939e9ba8216c
Yonder Peña <djyonder@gmail.com>
2021-11-29 10:34:47 -0500

    componentes con state vs sin state igual los controlados y los no

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const  ExpenseItem = (props) => {
-  //function
-  console.log("cualquier cosa")
-  const [title, setTitle] = useState(props.title);
-  // useState();
-  // let title = props.title;
-
-  const clickHandler = () => {
-    setTitle("Update!");
-    console.log(title);
-  }
-
+  // useState();  // let title = props.title;
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
